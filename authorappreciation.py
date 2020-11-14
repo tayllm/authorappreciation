6531e837b7afdeb90c7e3e6c82deffd474a760eb
tayllm <56044586+tayllm@users.noreply.github.com>
2020-11-14 15:28:56 -0500

    Update authorappreciation.py

--- authorappreciation.py
+++ authorappreciation.py
@@ -6,7 +6,7 @@ import urllib
 
 #client = MongoClient('mongodb://heroku_8pzzctpn:hnnu9ch1h6j867d2eiq613bbnl@ds149201.mlab.com:49201/heroku_8pzzctpn?retryWrites=false')
 mongo_uri = "mongodb+srv://heroku_8pzzctpn:" + urllib.parse.quote("k@DZdbdSWd3BYCs") + "@cluster-8pzzctpn.0a7uv.mongodb.net/heroku_8pzzctpn?retryWrites=true&w=majority"
-client = pymongo.MongoClient(mongo_uri)
+client = MongoClient(mongo_uri)
 #client = MongoClient('mongodb+srv://heroku_8pzzctpn:k@DZdbdSWd3BYCs@cluster-8pzzctpn.0a7uv.mongodb.net/heroku_8pzzctpn?retryWrites=true&w=majority')
 db = client.get_default_database()
 
